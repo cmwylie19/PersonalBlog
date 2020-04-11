91a634aa06614f6152c76fee0bccc9d95b1f7087
cmwylie19 <cwylie@redhat.com>
2020-04-11 01:33:46 -0400

    styles

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -5,14 +5,15 @@ Wordpress2016.overrideThemeStyles = () => {
   return {
     "a.gatsby-resp-image-link": {
       boxShadow: `none`,
-    },
+    }
   }
 }
 
 delete Wordpress2016.googleFonts
 
-const typography = new Typography(Wordpress2016)
 
+const typography = new Typography(Wordpress2016)
+console.log(typography.toJSON())
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
   typography.injectStyles()
