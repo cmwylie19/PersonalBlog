dde762522c5b7b68f9aff7e2917e82fa602430b3
Casey Wylie <cmwylie19@gmail.com>
2021-10-26 12:20:22 -0400

    use secret

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -22,7 +22,7 @@ export default class API {
   logURL() {
     console.log("URL IS: " + this.url)
     console.log("Secret is "+process.env.API_SECRET)
-    console.log("REACT_APP_Secret is "+process.env.API_SECRET)
+    console.log("REACT_APP_Secret is "+process.env.REACT_APP_API_SECRET)
   }
   async recordPost(post) {
     const body = {
